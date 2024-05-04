587c515ba7c9d18af85a8d9f30474738efb01df0
Matthew Ashcroft <matt.ashcroft03@gmail.com>
2024-05-04 17:42:30 +0100

    updated tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <h2>Hiii Hello Hiiiiiii</h2>
+  <div class="bg-neutral-900 h-svh py-20">
+    <h1 class="text-white text-center font-[Rodondo] text-8xl drop-shadow-sm">Rain World Geoguesser</h1>
   </div>
 </template>
+<script setup lang="ts">
+</script>
